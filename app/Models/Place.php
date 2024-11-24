3cb309bc0e85dd37f2b8c7b7d70ae6cded0fc60a
Richard Fletcher <richard@artumi.com>
2024-11-24 20:59:31 +0000

    Initial commit

---
+++ app/Models/Place.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Place extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'location',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'published_at' => 'timestamp',
+    ];
+}
